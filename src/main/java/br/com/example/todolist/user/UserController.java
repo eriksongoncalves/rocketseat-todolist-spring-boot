68961376dc3ad7fe711139ca211790785130d036
Erikson Gonçalves Bezerra <erikson.g.bezerra@bradesco.com.br>
2023-10-12 11:28:34 -0300

    hash password

--- src/main/java/br/com/example/todolist/user/UserController.java
+++ src/main/java/br/com/example/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.example.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,9 @@ public class UserController {
         if (user != null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não encontrado!");
         }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashed);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
