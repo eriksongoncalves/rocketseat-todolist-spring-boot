7d4fa7a12304ef77d7155b5be7302a016d68b567
Erikson Gonçalves Bezerra <erikson.g.bezerra@bradesco.com.br>
2023-10-12 11:21:47 -0300

    add create user

--- src/main/java/br/com/example/todolist/user/UserController.java
+++ src/main/java/br/com/example/todolist/user/UserController.java
@@ -1,12 +1,26 @@
 package br.com.example.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel user){
-        System.out.println(user.getName());
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não encontrado!");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
