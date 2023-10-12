eebca84e1946a9465edae47a44e81c9f514f4dba
Erikson Gonçalves Bezerra <erikson.g.bezerra@bradesco.com.br>
2023-10-12 10:22:21 -0300

    add lombok

--- src/main/java/br/com/example/todolist/user/UserController.java
+++ src/main/java/br/com/example/todolist/user/UserController.java
@@ -1,14 +1,11 @@
 package br.com.example.todolist.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    @GetMapping("/")
+    @PostMapping("/")
     public void create(@RequestBody UserModel user){
         System.out.println(user.getName());
     }
